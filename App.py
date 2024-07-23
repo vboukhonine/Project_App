b67c710656937623031dc274f9b921a4a2d27448
Vera Boukhonine <veraboukhonine123@gmail.com>
2024-07-22 19:50:41 -0700

    Project files

---
+++ App.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+# flask --app App --debug run
+API_KEY = "14b2d074b0a84e4dacf83ae3f7036d38"
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "csumb-otter"
+bootstrap = Bootstrap5(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    query = request.form["query"]
+    url = f"https://api.spoonacular.com/recipes/complexSearch?query={query}&apiKey={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return render_template("index.html", recipes=data["results"])
+
+
+@app.route("/recipe/<int:recipe_id>")
+def recipe(recipe_id):
+    url = (
+        f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={API_KEY}"
+    )
+    try:
+        response = requests.get(url)
+        recipe_data = response.json()
+        return render_template("recipe.html", recipe=recipe_data)
+    except Exception as e:
+        print(e)
+        return "An error occurred. Please try again."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
